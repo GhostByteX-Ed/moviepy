b5f0c3869b19b1bf1ec050fb34aa38d95ec1ca86
GhostByteX-Ed <141905460+GhostByteX-Ed@users.noreply.github.com>
2023-08-23 16:40:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from moviepy import *
+# Import everything needed to edit video clips
+from moviepy.editor import *
 
-video = VideoFileClip("myHolidays.mp4").subclip(50,60)
+# loading video dsa gfg intro video
+clip = VideoFileClip("video.webm")
 
-# Make the text. Many more options are available.
-txt_clip = ( TextClip("My Holidays 2013",fontsize=70,color='white')
-             .with_position('center')
-             .with_duration(10) )
+# clipping of the video
+# getting video for only starting 10 seconds
+clip = clip.subclip(0, 10)
 
-result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
-result.write_videofile("myHolidays_edited.webm",fps=25) # Many options...
+# rotating video by 180 degree
+clip = clip.rotate(180)
+
+# Reduce the audio volume (volume x 0.5)
+clip = clip.volumex(0.5)
+
+# showing clip
+clip.ipython_display(width = 280)
