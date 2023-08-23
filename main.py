38678a3787e7224510c850f1896d793bde102381
GhostByteX-Ed <141905460+GhostByteX-Ed@users.noreply.github.com>
2023-08-23 16:58:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-# Import everything needed to edit video clips
-from moviepy.editor import *
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+from moviepy.editor import VideoFileClip
 
-# loading video dsa gfg intro video
-clip = VideoFileClip("file_example_WEBM_480_900KB.webm")
+def create_text_image(text, font_size, color, background_color, width, height):
+    image = Image.new("RGB", (width, height), background_color)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("arial.ttf", size=50)
+    _, _, w, h = draw.textbbox((0, 0), text)
+    x = (width - w*4) // 2 
+    y = (height - h*3 ) // 2
+    draw.text((x, y), text, font=font, fill=color)
+    return image
 
-# clipping of the video
-# getting video for only starting 10 seconds
-clip = clip.subclip(0, 10)
+# Step 1: Load the video clip from URL
+video_url = "https://file-examples.com/storage/fe3b4f721f64dfeffa49f02/2020/03/file_example_WEBM_480_900KB.webm"
+video_clip = VideoFileClip(video_url)
 
-# rotating video by 180 degree
-clip = clip.rotate(180)
+text = "Welcome to Educative!"
+font_size = 50
+text_color = (255, 255, 255)  # White color
+background_color = (0, 0, 0)  # Black background
+width, height = video_clip.size
+text_image = create_text_image(text, font_size, text_color, background_color, width, height)
 
-# Reduce the audio volume (volume x 0.5)
-clip = clip.volumex(0.5)
+# Convert the Pillow Image to a NumPy array
+text_np = np.array(text_image)
 
-# showing clip
-clip.ipython_display(width = 280)
+final_clip = video_clip.fl_image(lambda frame: cv2.addWeighted(frame, 1, text_np, 0.5, 0))
+
+for frame in final_clip.iter_frames(fps=final_clip.fps):
+    cv2.imshow("Final Clip", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
